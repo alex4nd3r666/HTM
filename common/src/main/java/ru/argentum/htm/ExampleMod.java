d5c248d4e82fbf78aeafd96381ed52a2e70544dc
Alexλnd3r <lexa2268spb@gmail.com>
2025-10-27 22:04:58 +0300

    INIT1

--- common/src/main/java/ru/argentum/htm/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package ru.argentum.htm;
-
-public class ExampleMod {
-    public static final String MOD_ID = "htm";
-
-    public static void init() {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/ru/argentum/htm/content/items/ModItems.java
@@ -0,0 +1,4 @@
+package ru.argentum.htm.content.items;
+
+public class ModItems {
+}
